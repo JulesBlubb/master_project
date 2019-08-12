ad3c397ac24168430335eb34f99acbc7d9bf5d79
JulesBlubb <juliane-weilbach@web.de>
2019-08-12 07:53:29 +0200

    read in data automatically & data augmentation

--- intentionrf/src/randomforest.py
+++ intentionrf/src/randomforest.py
@@ -2,22 +2,25 @@
 
 from sklearn.ensemble import RandomForestClassifier
 
-import rospy
 import numpy as np
-import rosbag
 import pandas as pd
 import ast
-import numpy as np
+from numpy.linalg import inv
+import glob
+import os
+# functools
 
 # Read in Files
 def readIn(fileName):
     lineList = [line.rstrip('\n') for line in open(fileName)]
     return lineList
 
+
 def divideFeature(openFile):
     pos = openFile[0::2]
     vel = openFile[1::2]
-    return pos,vel
+    return pos, vel
+
 
 def cutOffString(feat):
     result = []
@@ -30,11 +33,11 @@ def cutOffString(feat):
 def convertString(string_list):
     result = []
     for s in string_list:
-        #print(s)
+        # print(s)
         x = ast.literal_eval(s)
         converted = map(float, x)
         result.append(converted)
-        #print(converted)
+        # print(converted)
     return result
 
 
@@ -44,37 +47,47 @@ def createRaws(fileName):
     raw_points_vel = convertString(cutOffString(temp[1]))
     return raw_points_pos, raw_points_vel
 
-def createDf(fileName,label):
+def createDf(fileName, label):
     temp = createRaws(fileName)
-    X = pd.DataFrame(np.column_stack([temp[0], temp[1]]), columns = ['pos_x', 'pos_y', 'pos_y', 'vel_x', 'vel_y', 'vel_z'])
+    X = pd.DataFrame(np.column_stack([temp[0], temp[1]]), columns=['pos_x', 'pos_y', 'pos_z',
+                                                                   'vel_x', 'vel_y', 'vel_z'])
     X['label'] = label
     return X
 
-button_In_34 = "transformed/button_int_2019-07-03-13-28-34.txt"
-button_In_11 = "transformed/button_int_2019-07-03-13-56-11.txt"
-button_In_40 = "transformed/button_int_2019-07-03-13-56-40.txt"
+def createOneDataFrame():
+    path = 'transformed/'
+    X_temp = []
+    for filename in glob.glob(os.path.join(path, '*.txt')):
+        first_fileword = os.path.basename(filename).split('_')[0]
+        # Interaction gets label 1 and Passing label 0
+        label = 0 if first_fileword == 'passing' else 1
+        X_temp.append(createDf(filename, label))
+    X_temp = pd.concat(X_temp)
+    return X_temp
 
-voice_14 = "transformed/voice_int_2019-07-03-13-57-14.txt"
-voice_46 = "transformed/voice_int_2019-07-03-13-57-46.txt"
+# data augmentation, invert y values to mirror direction human is coming frome
+def mirror(dat):
+    y_values_pos = np.matrix(dat['pos_y'])
+    y_values_vel = np.matrix(dat['vel_y'])
+    return(y_values_pos.I, y_values_vel.I)
 
-passing_39 = "transformed/passing_2019-07-03-13-55-39.txt"
-passing_00 = "transformed/passing_2019-07-03-13-55-00.txt"
-
-# Interaction gets label 1 and Passing label 0
-X_temp = pd.concat([(createDf(button_In_34, 1)), (createDf(passing_00, 0)), (createDf(passing_39, 0)), (createDf(button_In_11, 1)), (createDf(button_In_40, 1)), (createDf(voice_14, 1)) ])
-
+X_temp = createOneDataFrame()
 X = X_temp.loc[:, X_temp.columns != 'label']
-y = X_temp.iloc[:,-1]
+y = X_temp.iloc[:, -1]
+
+# add mirrored y_values
+X['inv_pos_y'] = mirror(X)[0]
+X['inv_vel_y'] = mirror(X)[1]
 
 
-# Create Random Forest
-forest = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=0, oob_score=True)
+# # Create Random Forest
+forest = RandomForestClassifier(n_estimators=700, random_state=0, oob_score=True)
 
-forest.fit(X,y)
+forest.fit(X, y)
 
-print('Score: ', forest.score(X, y))
+# # print('Score: ', forest.score(X, y))
 
-# Print Out of Bag Error (Mean Prediction Error, Error of unseen bootstrapped data) number of correct prediced rows from out of bag sample
+# # Print Out of Bag Error (Mean Prediction Error, Error of unseen bootstrapped data) number of correct prediced rows from out of bag sample
 print('OOB', forest.oob_score_)
 
 
@@ -82,7 +95,7 @@ print('OOB', forest.oob_score_)
 
 
 
-#print(clf.feature_importances_)
+ #print(clf.feature_importances_)
 
 
 
